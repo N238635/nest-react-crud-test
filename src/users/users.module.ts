7a2c655aa4d3c63db729813067a8d1b57a21118c
BuildTools <Nickers@mail.ru>
2020-05-10 15:27:43 +0500

    Email scalar added to UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersResolver } from './users.resolver';
 import { UserSchema } from './users.schema';
 import { UsersService } from './users.service';
+import { EmailScalar } from './email.scalar-type';
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
-    providers: [UsersResolver, UsersService],
+    providers: [UsersResolver, UsersService, EmailScalar],
 })
 export class UsersModule { }
